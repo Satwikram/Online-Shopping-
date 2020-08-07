114926756ef31d2f46656bd3cdbe8340a6e0abed
Satwik Ram <satwikram29@gmail.com>
2020-08-07 15:29:28 +0530

    fix updated

--- shopping/settings.py
+++ shopping/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '##pu@(*=u3od%4#^nbzsnaz!bb6-+nb+^&y!tn@3==r6%0-nai'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +87,7 @@ DATABASES = {
         'NAME' : 'shop',
         'HOST' : 'localhost',
         'USER' : 'postgres',
-        'PASSWORD' : 'pass6671',
+        'PASSWORD' : password,
 
 
     }
@@ -153,5 +154,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'satwikram29@gmail.com' # Your Gmail Account
-EMAIL_HOST_PASSWORD = '66716671' # Gmail password
+EMAIL_HOST_USER = mail # Your Gmail Account
+EMAIL_HOST_PASSWORD = gpassword # Gmail password
