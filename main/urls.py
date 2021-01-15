1d6c2eded87ee1628ff7aabd53cedb8ff1c43cc3
Satwik Ram <satwikram29@gmail.com>
2021-01-15 13:49:19 +0530

    Add to cart url

--- main/urls.py
+++ main/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
                 path('sell', views.sell, name = 'sell'),
                 path('buy/<str:slug>', views.buy, name = 'buy'),
                 path('search', views.search, name = "search"),
-                path('cart/<str:slug>', views.cart, name = 'cart'),
+                path('addcart/<str:slug>', views.addcart, name = 'addcart'),
                 path('checkout', views.checkout, name = 'checkout'),
 
                 # Django RestAPI URLS
